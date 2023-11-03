c3ae52d8e5943f2135dafa1c5c04d0ca85c49899
pranaymoluguri <pranaymoluguri@gmail.com>
2023-11-03 16:58:05 +0530

    Guess2

--- GuessGame.py
+++ GuessGame.py
@@ -2,10 +2,12 @@ import numpy as np
 
 r=np.random.randint(100)
 i=0
+c=0
 while i<1:
     x=int(input("Guess the number between 0 and 100 : "))
+    c+=1
     if(x==r):
-        print("Perfect Catch !!")
+        print("Nice Catch !! The number was: ",r,"& No of attempts: ",c)
         break
     elif(x<r):
         print("Increase the number... ")
